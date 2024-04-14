57dc2f962385f354e8213b425fa280b4fc5845b6
kirill <shkolenko02-03-2004@mail.ru>
2024-04-14 10:19:43 +0300

    Added execl report

--- routing/v1/recognition.py
+++ routing/v1/recognition.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+import io
+
+import pandas as pd
+from fastapi import APIRouter, Response
+from starlette.responses import StreamingResponse
 
 from ml.conversation_pipeline import result_pipeline
 from schemas.recognition import RecognizeRequest, RecognizeResponse
@@ -31,3 +35,31 @@ async def get_answer(req: RecognizeRequest):
         product=product,
         rule=rules,
     )
+
+
+@router.post(
+    "/report",
+    description="getting the answer in excel format",
+)
+async def get_report(req: RecognizeRequest):
+    back_side = base642nparray(req.back_side)
+    front_side = base642nparray(req.front_side)
+
+    pipeline_json, rules = result_pipeline([back_side, front_side])
+
+    try:
+        pipeline_json = str2json(pipeline_json)
+
+        df = pd.DataFrame(pipeline_json)
+        buffer = io.BytesIO()
+        with pd.ExcelWriter(buffer) as writer:
+            df.to_excel(writer, index=False)
+
+        return StreamingResponse(
+            io.BytesIO(buffer.getvalue()),
+            media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            headers={"Content-Disposition": f"attachment; filename=data.csv"}
+        )
+
+    except Exception as e:
+        raise ValueError(f"error json dumping: {e}")
